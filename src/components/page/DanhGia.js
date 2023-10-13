5ca14915c0f4eb6f7dfa7d3a0748d8348125fd65
NguyenNgocMinh6868 <118373767+NguyenNgocMinh6868@users.noreply.github.com>
2023-10-13 09:59:37 +0700

    up

--- src/components/page/DanhGia.js
+++ src/components/page/DanhGia.js
@@ -1,7 +1,65 @@
-import React from "react";
+
+import React, { useState } from "react";
+import TableComponent from "../form/TableCustom";
 
 const DanhGia = () => {
-  return <div>DanhGia</div>;
+  const columns = [
+    {
+      title: "Khách hàng",
+      dataIndex: "ten",
+      key: "ten",
+    },
+    {
+      title: "Thời gian",
+      dataIndex: "time",
+      key: "time",
+    },
+    {
+      title: "Phân loại hàng",
+      dataIndex: "phanLoai",
+      key: "phanLoai",
+    },
+    {
+      title: "Đánh giá",
+      dataIndex: "danhGia",
+      key: "danhGia",
+    },
+    {
+      title: "Hình ảnh",
+      dataIndex: "anh",
+      key: "anh",
+    },
+    {
+      title: "Phản hồi",
+      dataIndex: "phanHoi",
+      key: "phanHoi",
+    },
+  ];
+  const dataSource = [
+    {
+      key: "1",
+      ten: "Nguyen Ngoc Minh",
+      time: "12/10/2023",
+      phanLoai: "Ao nike the thao",
+      danhGia: "10/10",
+      anh: "ai đó giúp t đi t méo bt làm phần ảnh",
+      phanHoi: "Hang tot, chat lieu mat me"
+    },
+    {
+      key: "2",
+      ten: "Nguyen Ngoc Minh",
+      time: "12/10/2023",
+      phanLoai: "Ao nike the thao",
+      danhGia: "10/10",
+      anh: "ai đó giúp t đi t méo bt làm phần ảnh",
+      phanHoi: "Hang tot, chat lieu mat me"
+    },
+  ];
+  return (
+    <>
+      <TableComponent columns={columns} dataSource={dataSource} />
+    </>
+  );
 };
 
 export default DanhGia;
