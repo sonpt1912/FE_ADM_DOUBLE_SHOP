3e40392f56431b72bfad3821ceb142e883e9d19f
Pham Thanh Son (FIS PBDXGHN) <sonpt51@fpt.com>
2023-10-13 08:54:35 +0700

    up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,13 +13,28 @@ import {
 } from "@ant-design/icons";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChartSimple } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChartSimple,
+  faUsers,
+  faShirt,
+  faGifts,
+  faMoneyCheck,
+  faUserGroup,
+  faEnvelopeOpenText,
+  faReceipt,
+  faRankingStar,
+  faShop,
+  faEnvelope,
+  faComments,
+  faArrowRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 import { Menu, Layout } from "antd";
 import { useNavigate } from "react-router-dom";
 import "../styles/Navbar.css";
 
 import Logo from "../assets/logo.png";
+import { size } from "lodash";
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
@@ -47,36 +62,38 @@ const Navbar = ({ collapsed }) => {
         <Menu.Item
           className="menu-item"
           key="/thongKe"
-          icon={<CalendarOutlined />}
+          icon={<FontAwesomeIcon icon={faChartSimple} />}
         >
-          <FontAwesomeIcon icon={faChartSimple} />
           Thống Kê
         </Menu.Item>
         <Menu.Item
           className="menu-item"
           key="/banHang"
-          icon={<DollarOutlined />}
+          icon={<FontAwesomeIcon icon={faShop} />}
         >
           Bán Hàng
         </Menu.Item>
         <Menu.Item
           className="menu-item"
           key="/chiTietDonHang"
-          icon={<InfoCircleOutlined />}
+          icon={<FontAwesomeIcon icon={faReceipt} />}
         >
           Chi tiết đơn hàng
         </Menu.Item>
         <Menu.Item
           className="menu-item"
           key="/khachHang"
-          icon={<TeamOutlined />}
+          icon={<FontAwesomeIcon icon={faUsers} />}
         >
           Khách Hàng
         </Menu.Item>
-        <SubMenu icon={<SkinOutlined />} title="Sản Phẩm">
-          <Menu.Item key="/pham">Phạm</Menu.Item>
-          <Menu.Item key="/thanh">Thanh</Menu.Item>
-          <Menu.Item key="/son">Sơn</Menu.Item>
+        <SubMenu icon={<FontAwesomeIcon icon={faShirt} />} title="Sản Phẩm">
+          <Menu.Item key="/Ao">Áo</Menu.Item>
+          <Menu.Item key="/Mau">Màu</Menu.Item>
+          <Menu.Item key="/KichCo">Kích cỡ</Menu.Item>
+          <Menu.Item key="/CoAo">Cổ áo</Menu.Item>
+          <Menu.Item key="/Chatlieu">Chất liệu</Menu.Item>
+          {/* <Menu.Item key="/">Sơn</Menu.Item> */}
         </SubMenu>
         <Menu.Item className="menu-item" key="/danhGia" icon={<EditOutlined />}>
           Đánh Giá
@@ -84,30 +101,53 @@ const Navbar = ({ collapsed }) => {
         <Menu.Item
           className="menu-item"
           key="/voucher"
-          icon={<NumberOutlined />}
+          icon={<FontAwesomeIcon icon={faMoneyCheck} />}
         >
           Voucher
         </Menu.Item>
         <Menu.Item
           className="menu-item"
+          key="/hang"
+          icon={<FontAwesomeIcon icon={faRankingStar} />}
+        >
+          Hạng
+        </Menu.Item>
+        <Menu.Item
+          className="menu-item"
           key="/khuyenMai"
-          icon={<GiftOutlined />}
+          icon={<FontAwesomeIcon icon={faGifts} />}
         >
           Khuyến Mãi
         </Menu.Item>
         <Menu.Item
           className="menu-item"
           key="/nhanVien"
-          icon={<UserOutlined />}
+          icon={<FontAwesomeIcon icon={faUserGroup} />}
         >
           Nhân Viên
         </Menu.Item>
-        <Menu.Item
-          className="menu-item"
-          key="/lienHe"
-          icon={<CommentOutlined />}
+        <SubMenu
+          icon={<FontAwesomeIcon icon={faEnvelopeOpenText} />}
+          title="Liên Hệ"
         >
-          Liên Hệ
+          <Menu.Item
+            key="/pham"
+            className="menu-item"
+            icon={<FontAwesomeIcon icon={faComments} />}
+          >
+            Chat trực tuyến
+          </Menu.Item>
+          <Menu.Item
+            className="menu-item"
+            key="/thanh"
+            icon={<FontAwesomeIcon icon={faEnvelope} />}
+          >
+            Gửi email
+          </Menu.Item>
+        </SubMenu>
+
+        <Menu.Item icon={<FontAwesomeIcon icon={faArrowRightFromBracket} />}>
+          Đăng xuất
         </Menu.Item>
       </Menu>
     </Sider>
