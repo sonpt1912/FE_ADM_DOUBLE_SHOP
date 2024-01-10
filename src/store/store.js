20f0a116f8c3ccaa7a63d0f7dfff4c1da78fbb43
Huongnt <nguyenthihuong011203@gmail.com>
2024-01-10 23:32:10 +0700

    update store

--- src/store/store.js
+++ src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import KhachHangReducer from "./slice/KhachHangReducer";
 import RankReducer from "./slice/RankReducer";
 import KichCoReducer from "./slice/KichCoReducer";
+import MauReducer from "./slice/MauReducer";
 const store = configureStore({
   reducer: {
     khachHang : KhachHangReducer,
     rank : RankReducer,
-    size : KichCoReducer
+    size : KichCoReducer,
+    color: MauReducer
   },
 });
 
